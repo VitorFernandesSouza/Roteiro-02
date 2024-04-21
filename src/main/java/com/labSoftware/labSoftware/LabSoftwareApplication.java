27fa7508eb54357eab93c8565444b39dff130624
Vitor Fernandes de Souza <111388245+VitorFernandesSouza@users.noreply.github.com>
2024-04-21 20:50:36 -0300

    Entrega

---
+++ src/main/java/com/labSoftware/labSoftware/LabSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.labSoftware.labSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labSoftware/labSoftware/LabSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labSoftware.labSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
