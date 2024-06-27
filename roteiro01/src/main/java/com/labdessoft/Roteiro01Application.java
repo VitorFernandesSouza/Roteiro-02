5c85d988c8c6198917e2eb3b7b54e179666fdd46
VitorFernandesSouza <vifersouza@gmail.com>
2024-06-26 22:51:25 -0300

    v

--- roteiro01/src/main/java/com/labdessoft/Roteiro01Application.java
+++ roteiro01/src/main/java/com/labdessoft/Roteiro01Application.java
@@ -1,4 +1,5 @@
-package com.labdessoft;
+package com.labdessoft.roteiro01;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
