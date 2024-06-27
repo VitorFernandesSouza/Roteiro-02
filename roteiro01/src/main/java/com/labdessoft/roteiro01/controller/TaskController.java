227f8d1adad40f189ec2fe6477ea25e0327c00f0
VitorFernandesSouza <vifersouza@gmail.com>
2024-06-26 22:26:27 -0300

    v

---
+++ roteiro01/src/main/java/com/labdessoft/roteiro01/controller/TaskController.java
@@ -0,0 +1,64 @@
+package com.labdessoft.roteiro01.controller;
+
+import com.labdessoft.roteiro01.entity.Task;
+import com.labdessoft.roteiro01.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    @Operation(summary = "Lista todas as tarefas da lista")
+    public ResponseEntity<List<Task>> listAll() {
+        List<Task> taskList = taskService.listAllTasks();
+        if (taskList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(taskList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Listar tarefa por id")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> task = taskService.getTaskById(id);
+        return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @Operation(summary = "Criar uma nova tarefa")
+    public ResponseEntity<Task> createTask(@RequestBody Task newTask) {
+        try {
+            Task task = taskService.createTask(newTask);
+            return ResponseEntity.status(HttpStatus.CREATED).body(task);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Editar tarefa")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Optional<Task> task = taskService.updateTask(id, updatedTask);
+        return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar Tarefa")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        if (taskService.deleteTask(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
